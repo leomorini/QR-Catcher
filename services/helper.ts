312eeffea718ec673df5f40fd70b33e3710593af
Leonardo Morini <leo.morini.nn@gmail.com>
2024-04-16 10:15:19 -0300

    add test image + fix helper url regex

--- services/helper.ts
+++ services/helper.ts
@@ -7,16 +7,8 @@ import { LinkInterface } from "./interfaces";
  * Checks if a string is a valid link
  */
 export function validURL(str: string) {
-  var pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  );
-  return !!pattern.test(str);
+  const regex = /^http:\/\/.+/i;
+  return !!regex.test(str);
 }
 
 /**
